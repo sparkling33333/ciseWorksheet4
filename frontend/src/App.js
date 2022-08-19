1f73665c219aff94ef3b858c7e99d7f1cf555fa4
Chris Y <chris_young@tutanota.com>
2022-08-19 21:45:35 +1200

    got 404 working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  Route, Routes,
+  NavLink,
+  BrowserRouter as Router 
+} from "react-router-dom";
+
+import Home from "./pages/Home";
+import SEPractice from "./pages/SE-Practice";
+import SubmitArticle from "./pages/Submit-Article"; 
+import NotFoundPage from "./pages/404";
+
+const App = () =>  {
+  
+    return (
+        <Router>
+        <div>
+          <h1>Software Engineering Empirical Evidence Database (SEED)</h1>
+            <ul className="header">
+                <li><NavLink exact to = "/">Home</NavLink></li>
+                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
+                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
+            </ul>
+          <div className="content">
+            
+          <Routes>
+              <Route exact path="/" element={< Home />} />
+              <Route  path="/SEPractice" element={< SEPractice />} />
+              <Route  path="/SubmitArticle" element={< SubmitArticle />} />
+              <Route path="/404" element={< NotFoundPage />} />
+              <Route path="*" element={< NotFoundPage />} />
+
+
+            </Routes>
+    
+
+          </div>
+        </div>
+        </Router>
+    );
 }
-
+ 
 export default App;
---
+++ frontend/src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react";
+ 
+const NotFoundPage = () =>  {
+     return (
+      <div>
+        <h2>404 - Page Not Found</h2>
+            <p>Select another page.</p>
+        </div>
+    );
+}
+ 
+export default NotFoundPage;
